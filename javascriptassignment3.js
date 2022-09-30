5544b4d63fa3d89199994e196bdcd03dce302301
DavidJC1105 <112865062+DavidJC1105@users.noreply.github.com>
2022-09-30 12:32:32 +0100

    Add files via upload

---
+++ javascriptassignment3.js
@@ -0,0 +1,33 @@
+//Assignment 3
+
+//Question 1='hello',-88.8 and 5
+
+//Question 2= 'spam'is a string and spam is a variable
+
+//question 3= numbers,strings anf booleans
+
+//Question 4= 21
+
+//Question 5= part 1=spamspamspam  part 2=error
+
+//Question 6= 
+let yup=prompt("Enter something")
+if(yup==1){
+    console.log("Hello")
+}
+if(yup=="2"){
+    console.log("Howdy")
+}
+else(console.log("Greetings"))
+
+//Question 7= first 3 have capital letters and the fourth is missing the . between console and log
+
+/*Question 8
+1= error
+2= Hello World will appear in console
+3= error
+4= Hello World will appear in console
+5= Nothing will appear in the console
+6= error
+7= Hello World and Welcome to Javascript will appear in console
+8= Nothing will appear because its a comment
